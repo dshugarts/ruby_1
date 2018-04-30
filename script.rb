f368de0e8e3ef3d617f45fcd18c9925d10a1a506
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-04-30 18:27:09 -0500

    even or odd

--- script.rb
+++ script.rb
@@ -6,12 +6,18 @@ print "What's your last name? "
 last_name = gets.chomp
 last_name.capitalize!
 
-print "What city are you from? "
-city = gets.chomp
-city.capitalize!
+print "Choose a number? "
+val = gets.chomp.to_i
 
-print "What state or province are you from? "
-state = gets.chomp
-state.upcase!
+def even_or_odd(val)
+    if val % 2 == 0
+       return "#{val} is an even number"
+    end
+    
+    if val % 2 == 1
+       return "#{val} is an odd number"
+    end
+ end
 
-puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
+puts "Your name is #{first_name} #{last_name} and you chose number #{val}.  "
+puts even_or_odd(val);
