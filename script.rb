c0512a6d8308a76caac65a002d1520923bd7a929
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-04-30 22:33:57 -0500

    half of solo form

--- script.rb
+++ script.rb
@@ -6,18 +6,70 @@ print "What's your last name? "
 last_name = gets.chomp
 last_name.capitalize!
 
-print "Choose a number? "
-val = gets.chomp.to_i
+print "How old are you? "
+ageval = gets.chomp.to_i
+finalage = 0;
+    if ageval > 50
+        finalage = 0
+    end
+    if ageval <= 50
+        finalage = 2
+    end
 
-def even_or_odd(val)
-    if val % 2 == 0
-       return "#{val} is an even number"
+print "What is your HDL value? "
+hdlval = gets.chomp.to_i
+finalhdl = 0;
+    if hdlval > 60
+        finalhdl = 2
     end
-    
-    if val % 2 == 1
-       return "#{val} is an odd number"
+    if hdlval <= 60 && hdlval >= 40
+        finalhdl = 1
     end
+    if hdlval < 40
+        finalhdl = 0
+    end
+
+print "What is your LDL value? "
+    ldlval = gets.chomp.to_i
+    finalldl = 0;
+        if ldlval < 100
+            finalldl = 2
+        end
+        if ldlval > 99 && ldlval < 130
+            finalldl = 1
+        end
+        if ldlval > 129
+            finalldl = 0
+        end
+
+print "What is your Glucose value? "
+    gluval = gets.chomp.to_i
+    finalglu = 0;
+            if gluval < 100
+                finalglu = 2
+            end
+            if gluval > 99 && gluval < 126
+                finalglu = 1
+            end
+            if gluval > 125
+                finalglu = 0
+            end
+
+print "How many hours per night do you sleep? "
+    slpval = gets.chomp.to_i
+    finalslp = 0;
+        if slpval >= 7
+            finalslp = 2
+        end
+        if slpval <= 6
+            finalslp = 0
+        end
+
+
+def heart_score(first_name, last_name, finalage, finalglu, finalhdl, finalldl, finalslp)
+    final_heart = finalage + finalglu + finalhdl + finalldl + finalslp
+    return "Your name is #{first_name} #{last_name} and you scored #{final_heart} healthy heart points out of 10!"
  end
 
-puts "Your name is #{first_name} #{last_name} and you chose number #{val}.  "
-puts even_or_odd(val);
+puts heart_score(first_name, last_name, finalage, finalglu, finalhdl, finalldl, finalslp);
+
